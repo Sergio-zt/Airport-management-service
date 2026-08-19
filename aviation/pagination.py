1f450efd7f144186dd5f3c765763b6aee4af1d2d
Sergio-zt <fop.gso@gmail.com>
2026-08-19 12:41:44 -0400

    change pagination class to custom

---
+++ aviation/pagination.py
@@ -0,0 +1,6 @@
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10                  # Default page size
+    page_size_query_param = 'size'  # Allow users to change pagination size
+    max_page_size = 100             # Protection Against Excessively Large Requests
